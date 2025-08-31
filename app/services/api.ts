dd1927a13a57cce1885a17b704c8b115c587b6fe
Ken Santos <kenxsantos@gmail.com>
2025-09-01 00:17:11 +0800

    fix: remove default baseURL for API configuration

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,7 +2,7 @@ import { ChangeLogs, Task } from "@/interfaces";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/proxy",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
