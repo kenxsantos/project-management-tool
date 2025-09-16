e93a0f2d03e0bc094bb265a6e40cfc824e089655
Ken Santos <kenxsantos@gmail.com>
2025-09-16 23:08:39 +0800

    fix:  improve layout structure

--- app/(dashboard)/projects/[id]/layout.tsx
+++ app/(dashboard)/projects/[id]/layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import { cookies } from "next/headers"
-import { fetchAllProjects } from "@/app/services/api"
+import { fetchAllProjects } from "@/services/api"
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const cookieStore = await cookies()
@@ -12,9 +12,14 @@ export default async function Layout({ children }: { children: React.ReactNode }
     return (
         <SidebarProvider defaultOpen={defaultOpen}>
             <AppSidebar projects={projects} />
-            <main>
-                <SidebarTrigger />
-                {children}
+            <main className="w-full sm:flex justify-between p-4 mb-12 overflow-auto">
+                <div>
+                    <SidebarTrigger />
+                </div>
+                <div className="w-full space-y-4 mt-10">
+                    {children}
+                </div>
+                <div></div>
             </main>
         </SidebarProvider>
     )
