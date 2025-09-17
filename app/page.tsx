43a0c9950fdd049ef153d3bd0d72d7e7c3a7634e
Ken Santos <kenxsantos@gmail.com>
2025-09-18 01:31:16 +0800

    feat: implement motion effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { motion } from "motion/react";
+
 
 export default function Home() {
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-3 pb-20 gap-16 sm:p-20">
-            <main className="flex flex-col gap-4 row-start-2 items-center">
+            <motion.main
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.4,
+                    scale: { type: "spring", visualDuration: 0.2, bounce: 0.1 },
+                }}
+                className="flex flex-col gap-4 row-start-2 items-center">
                 <p className="text-5xl font-bold uppercase text-[#1b1b1b] text-center"> Project Management Tool</p>
                 <p className="text-base text-[#1b1b1b] text-center"> A simple drag and drop project management tool using dnd kit.</p>
                 <div className="flex gap-8">
@@ -15,7 +25,7 @@ export default function Home() {
                         <Button variant="outline">Sign Up</Button>
                     </Link>
                 </div>
-            </main>
+            </motion.main>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
                 <p className="text-sm">© 2025 Ken Santos. All rights reserved.</p>
             </footer>
